0453b770ed72b719013a7b7d9b248d06004485ff
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-06-06 02:02:15 -0500

    chore: Implementing styles with styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'; 
 import Navbar from './components/Navbar';
 import OrderForm from './components/OrderForm';
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0
+        font-family: 'Roboto Mono', monospace;
+    }
+`
 
 const App = () => {
     return(
         <div>
+            <GlobalStyle />
             <Navbar />
             <OrderForm />
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavbarContainer = styled.nav`
+    background-color: #40EC8E;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const NavbarTitle = styled.h1`
+    font-size: 1.5rem;
+    margin: .8rem 1.6rem;
+    font-family: 'Roboto Mono', monospace;
+`
+
+const NavbarList = styled.ul`
+    display: flex;
+    list-style: none;
+`
+
+const NavbarItem = styled.li`
+    margin: 0 2rem;
+` 
+
+const NavbarLink = styled.a`
+    font-size: 1.5rem;
+    color: black;
+    text-decoration: none;
+    font-family: 'Roboto Mono', monospace;
+`
 
 const Navbar = () => {
-    return(
-        <nav className="Navbar-Container">
-        <h1 className="Navbar-Title">Coffee Orders</h1>
-        <ul className="Navbar-List">
-            <li className="Navbar-Item">
-                <a className="Navbar-Item-Link" href="#" target="_blank">
-                    Home
-                </a>
-            </li>
-            <li className="Navbar-Item">
-                <a className="Navbar-Item-Link" href="https://github.com/ArianZambrano" target="_blank">
-                    About Me
-                </a>
-            </li>
-            <li className="Navbar-Item">
-                <a className="Navbar-Item-Link" href="https://github.com/ArianZambrano" target="blank">
-                    Contact
-                </a>
-            </li>
-        </ul>
-    </nav>
+    return (
+        <header>
+            <NavbarContainer className="Navbar-Container">
+                <NavbarTitle className="Navbar-Title">
+                    Coffee Orders
+                </NavbarTitle>
+                <NavbarList className="Navbar-List">
+                    <NavbarItem className="Navbar-Item">
+                        <NavbarLink className="Navbar-Item-Link" href="#" target="_blank">
+                            Home
+                        </NavbarLink>
+                    </NavbarItem>
+                    <NavbarItem className="Navbar-Item">
+                        <NavbarLink className="Navbar-Item-Link" href="https://github.com/ArianZambrano" target="_blank">
+                            About Me
+                        </NavbarLink>
+                    </NavbarItem>
+                    <NavbarItem className="Navbar-Item">
+                        <NavbarLink className="Navbar-Item-Link" href="https://github.com/ArianZambrano" target="blank">
+                            Contact
+                        </NavbarLink>
+                    </NavbarItem>
+                </NavbarList>
+            </NavbarContainer>
+        </header>
     )
 }
 
